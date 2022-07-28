750fd940edfde199aca2748a9174c6b33b51d865
SurHak <110163421+SurHak@users.noreply.github.com>
2022-07-28 16:50:28 +0400

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,16 @@
+let h = document.querySelector("#h");
+h.addEventListener('click', function(){
+    h.innerHTML = "HTML = 99%"
+});  
+
+let c = document.querySelector("#c");
+c.addEventListener('click', function(){
+    c.innerHTML = "CSS = 99%"
+});  
+
+
+let j = document.querySelector("#j");
+j.addEventListener('click', function(){
+    j.innerHTML = "JAVASCRIPT = 99%"
+});  
+
